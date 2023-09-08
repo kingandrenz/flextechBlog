95d72f1320c47e25949fbb8dbe37a3bd6dadbc27
andrenz <kanuchibueze@gmail.com>
2023-09-08 13:08:26 +0100

    new update

--- blog-app/server/app.js
+++ blog-app/server/app.js
@@ -1,58 +1,51 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+//epress app 
 const app = express();
 
-// connect to mongodb
-const dbUri = 'mongodb+srv://Andrenz:Akaka1na5@flextech-blog.m0d8jso.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(dbUri);
+// connect to mongodb
+const dbUri = 'mongodb+srv://Andrenz:Akaka1na5@flextech-blog.m0d8jso.mongodb.net/Flextech-blog?retryWrites=true&w=majority';
+mongoose.connect(dbUri)
+.then((result) => app.listen(port, () => console.log(`server is running on ${port}`)))
+.catch((err) => console.log(err))
 const port = 3000;
 
 // register a view engine
 app.set('view engine', 'ejs');
 
-app.listen(port, () => {
+/* app.listen(port, () => {
         console.log(`server is running on ${port}`);
-});
+}); */
 
-// middleware static files (css, imafes e.t.c)
+// middleware static files (css, images e.t.c)
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
 
+  // basic routes
 app.get('/', (req, res) => {
-	const  blogs = [
-  {
-    title: "How to Choose the Best Solar Panel Wattage for Your Situation",
-    snippet: "Learn how to estimate your energy needs, solar potential, and solar fraction, and how to choose the type, efficiency, and number of solar panels that suit your preferences and budget."
-  },
-  {
-    title: "The Benefits of Meditation for Stress Relief and Mental Health",
-    snippet: "Discover how meditation can help you cope with stress, anxiety, and depression, and how to practice different types of meditation techniques for optimal results."
-  },
-  {
-    title: "The Ultimate Guide to Traveling Around the World on a Budget",
-    snippet: "Find out how to plan your trip, save money on flights, accommodation, and transportation, and explore the best destinations and attractions in each continent."
-  },
-  {
-    title: "How to Start a Successful Blog in 2023: A Step-by-Step Guide",
-    snippet: "Learn how to choose a niche, domain name, and hosting service, how to design your blog, create content, and monetize your traffic, and how to grow your audience and brand."
-  },
-  {
-    title: "The Top 10 Programming Languages to Learn in 2023",
-    snippet: "Find out which programming languages are in high demand, easy to learn, and versatile, and how to choose the best one for your career goals and personal projects."
-  } 
-];
-
-	res.render('index', {title: 'Home', blogs});
+  res.redirect('/blogs');
 });
-
 
 app.get('/about', (req, res) => {
         res.render('about', {title: 'about'});
 });
 
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+  .then((result) => {
+    res.render('index', {title: 'All Blogs', blogs: result})
+  })
+  .catch((err) => {
+    console.log(err);
+    });
+
+})
 app.get('/blogs/create', (req, res) => {
 	res.render('create', {title: 'create'});
 });
