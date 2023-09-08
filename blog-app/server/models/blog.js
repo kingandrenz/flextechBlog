d724ce22afbe2cb240d1c159849a082c5dde8b88
andrenz <kanuchibueze@gmail.com>
2023-09-08 13:12:06 +0100

    new update

---
+++ blog-app/server/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const blogSchema = new schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+
+}, {timestamps: true});
+
+//models
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
